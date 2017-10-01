3138982ec35ad2251e83e3105a85a041e96c6562
JungwooKim92 <kimwoo0002@naver.com>
2017-10-01 20:04:44 +0900

    AR V-0.0.2

---
+++ Assets/script/Game/AR/AR_Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AR_Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider tan)
+    {
+
+        if (tan.tag == "AR_Enemy_Bullet")
+        {
+            Destroy(tan.gameObject);
+            //if (Now_HP <= 0)
+            //{
+            //    Destroy(this.gameObject);
+            //}
+        }
+    }
+}
---
+++ Assets/script/Game/AR/Cube/Cube_Shoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Shoot : MonoBehaviour {
+
+
+    public Transform shoot_pos = null;
+    void Start () {
+		
+	}
+    // Update is called once per frame
+    void Update()
+    {
+    }
+     
+    public Transform get_shoot_pos()
+    {
+        return shoot_pos;
+    }
+}
