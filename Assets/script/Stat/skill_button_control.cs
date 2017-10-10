4e44819b27d18fe5804b94b14f5cb12e345881fa
kitoha <kth004@naver.com>
2017-10-10 17:19:15 +0900

    stat complete

---
+++ Assets/script/Stat/skill_button_control.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skill_button_control : MonoBehaviour {
+
+	public GameObject skill_img;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void attack()
+	{
+		skill_img.GetComponent<Skill_img_control> ().attack = true;
+	}
+	public void deffence()
+	{
+		skill_img.GetComponent<Skill_img_control> ().attack = false;
+	}
+}
