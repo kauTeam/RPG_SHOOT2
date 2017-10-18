67ca4650da91d32f9912108a6047cc0497108168
JungwooKim92 <kimwoo0002@naver.com>
2017-10-18 09:11:05 +0900

    AR 수정 1V

---
+++ Assets/script/Game/AR/AR_GameControll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AR_GameControll : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject Bullet;
+    public GameObject win;
+    public GameObject lose;
+    public int Bullet_count;
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (player == null)
+        {
+            lose.SetActive(true);
+        }
+        if(Bullet == null && Bullet_count == 20)
+        {
+            win.SetActive(true);
+        }
+    }
+}
