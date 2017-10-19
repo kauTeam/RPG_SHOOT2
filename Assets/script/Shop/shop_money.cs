be657f3a7fb710e87f33ca080bf6f98aafcd4924
zjvlzld <jongsik1995@naver.com>
2017-10-19 11:55:47 +0900

    shop_3/4_complete

---
+++ Assets/script/Shop/shop_money.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class shop_money : MonoBehaviour {
+
+    public Text money;
+	// Use this for initialization
+	void Start () {
+        money = money.GetComponent<Text>();
+		
+	}
+
+    void Awake()
+    {
+        money = money.GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        money.text = PlayerPrefs.GetInt("money").ToString();
+	}
+}
