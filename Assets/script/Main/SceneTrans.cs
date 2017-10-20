90ad9924d70d184b26d7fdae60ee7940198b36f5
zjvlzld <jongsik1995@naver.com>
2017-10-20 10:06:23 +0900

    scene_trans_debug_complete

---
+++ Assets/script/Main/SceneTrans.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneTrans : MonoBehaviour {
+
+	public void Trans_Main()
+	{
+		SceneManager.LoadScene ("scene/Main");
+	}
+
+    public void Trans_Dungun()
+    {
+        SceneManager.LoadScene("scene/Game_prototype");
+    }
+
+    public void Trans_AR()
+    {
+        SceneManager.LoadScene("scene/AR");
+    }
+
+    public void Trans_Shop()
+    {
+        SceneManager.LoadScene("scene/shop");
+    }
+
+    public void Trans_Stat()
+    {
+        SceneManager.LoadScene("scene/Stat");
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
