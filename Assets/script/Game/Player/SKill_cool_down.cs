13ef54671ff895c06385982fca31c77062013458
zjvlzld <jongsik1995@naver.com>
2017-11-04 21:23:54 +0900

    skill_coolDown_complete by.jong

--- Assets/script/Game/Player/SKill_cool_down.cs
+++ Assets/script/Game/Player/SKill_cool_down.cs
@@ -1,16 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SKill_cool_down : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public List<Skill> skills;
+    public GameObject player;
+    public float[] sk1_cool = new float[6];
+    public float[] sk2_cool = new float[6];
+    float[] sk1_now_cool = new float[6];
+    float[] sk2_now_cool = new float[6];
+
+    public int[] deffence_mana = new int[6];
+    public int[] attack_mana = new int[6];
+    int now_character;
+
+    void Start()
+    {
+        now_character = PlayerPrefs.GetInt("now_character");
+        for(int i=0;i<6;i++)
+        {
+           sk1_now_cool[i]=sk1_cool[i]+1;
+           sk2_now_cool[i] = sk2_cool[i] + 1;
+        }
+        
+    }
+    private void Awake()
+    {
+        now_character = PlayerPrefs.GetInt("now_character");
+        for (int i = 0; i < 6; i++)
+        {
+            sk1_now_cool[i] = sk1_cool[i] + 1;
+            sk2_now_cool[i] = sk2_cool[i] + 1;
+        }
+
+
+    }
+    // Update is called once per frame
+    public void skill1()
+    {
+        if (player.transform.Find(now_character.ToString()).gameObject.GetComponent<Player_Status>().get_Now_MP() >= attack_mana[now_character])
+        {
+            if (sk1_now_cool[now_character] >= sk1_cool[now_character])
+            {
+                player.gameObject.GetComponent<use_skill>().use_skill1();
+                player.transform.Find(now_character.ToString()).gameObject.GetComponent<Player_Status>().use_mp(attack_mana[now_character]);
+                sk1_now_cool[now_character] = 0;
+            }
+        }
+    }
+    public void skill2()
+    {
+        if (player.transform.Find(now_character.ToString()).gameObject.GetComponent<Player_Status>().get_Now_MP() >= deffence_mana[now_character])
+        {
+            if (sk2_now_cool[now_character] >= sk2_cool[now_character])
+            {
+                player.gameObject.GetComponent<use_skill>().use_skill2();
+                player.transform.Find(now_character.ToString()).gameObject.GetComponent<Player_Status>().use_mp(deffence_mana[now_character]);
+                sk2_now_cool[now_character] = 0;
+            }
+        }
+    }
+    private void Update()
+    {
+        if (sk1_now_cool[now_character] < sk1_cool[now_character])
+        {
+            sk1_now_cool[now_character] += Time.deltaTime;
+            print((sk1_now_cool[now_character] / sk1_cool[now_character]).ToString());
+            skills[0].skillIcon.fillAmount = sk1_now_cool[now_character] / sk1_cool[now_character];
+        }
+        if (sk2_now_cool[now_character] < sk2_cool[now_character])
+        {
+            sk2_now_cool[now_character] += Time.deltaTime;
+            skills[1].skillIcon.fillAmount = sk2_now_cool[now_character] / sk2_cool[now_character];
+        }
+
+    }
+}
+
+[System.Serializable]
+public class Skill
+{
+    public Image skillIcon;
 }
--- Assets/script/Game/Player/use_skill.cs
+++ Assets/script/Game/Player/use_skill.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class use_skill : MonoBehaviour {
 
 
-    public int[] mp1 = new int[6];
-    public int[] mp2 = new int[6];
-
+    
     int character;
 	
     // Use this for initialization
@@ -28,97 +26,41 @@ public class use_skill : MonoBehaviour {
         switch(character)
         {
             case 0:
-                if(this.transform.Find("0").gameObject.GetComponent<Player_Status>().get_Now_MP()>= mp1[character])
-                {
                     //스킬구현
-                    this.transform.Find("0").gameObject.GetComponent<Player_Status>().use_mp(mp1[character]);
-                }
                 break;
             case 1:
-                if (this.transform.Find("1").gameObject.GetComponent<Player_Status>().get_Now_MP() >= mp1[character])
-                {
                     //스킬구현
-                    this.transform.Find("1").gameObject.GetComponent<Player_Status>().use_mp(mp1[character]);
-                }
                 break;
             case 2:
-                if (this.transform.Find("2").gameObject.GetComponent<Player_Status>().get_Now_MP() >= mp1[character])
-                {
                     //스킬구현
-                    this.transform.Find("2").gameObject.GetComponent<Player_Status>().use_mp(mp1[character]);
-                }
                 break;
             case 3:
-                if (this.transform.Find("3").gameObject.GetComponent<Player_Status>().get_Now_MP() >= mp1[character])
-                {
                     //스킬구현
-                    this.transform.Find("3").gameObject.GetComponent<Player_Status>().use_mp(mp1[character]);
-                }
                 break;
             case 4:
-                if (this.transform.Find("4").gameObject.GetComponent<Player_Status>().get_Now_MP() >= mp1[character])
-                {
                     //스킬구현
-                    this.transform.Find("4").gameObject.GetComponent<Player_Status>().use_mp(mp1[character]);
-                }
                 break;
             case 5:
-                if (this.transform.Find("5").gameObject.GetComponent<Player_Status>().get_Now_MP() >= mp1[character])
-                {
-                    //스킬구현
-                    this.transform.Find("5").gameObject.GetComponent<Player_Status>().use_mp(mp1[character]);
-                }
                 break;
         }
     }
 
     public void use_skill2()
     {
-        print(this.transform.Find("0").gameObject.GetComponent<Player_Status>().get_Now_MP());
-
+        
         switch (character)
         {
             case 0:
-                if (this.transform.Find("0").gameObject.GetComponent<Player_Status>().get_Now_MP() >= mp2[character])
-                {
-                    //스킬구현
-                    this.transform.Find("0").gameObject.GetComponent<Player_Status>().use_mp(mp2[character]);
-                }
                 break;
             case 1:
-                if (this.transform.Find("1").gameObject.GetComponent<Player_Status>().get_Now_MP() >= mp2[character])
-                {
-                    //스킬구현
-                    this.transform.Find("1").gameObject.GetComponent<Player_Status>().use_mp(mp2[character]);
-                }
                 break;
             case 2:
-                if (this.transform.Find("2").gameObject.GetComponent<Player_Status>().get_Now_MP() >= mp2[character])
-                {
-                    //스킬구현
-                    this.transform.Find("2").gameObject.GetComponent<Player_Status>().use_mp(mp2[character]);
-                }
                 break;
             case 3:
-                if (this.transform.Find("3").gameObject.GetComponent<Player_Status>().get_Now_MP() >= mp2[character])
-                {
-                    //스킬구현
-                    this.transform.Find("3").gameObject.GetComponent<Player_Status>().use_mp(mp2[character]);
-                }
                 break;
             case 4:
-                if (this.transform.Find("4").gameObject.GetComponent<Player_Status>().get_Now_MP() >= mp2[character])
-                {
-                    //스킬구현
-                    this.transform.Find("4").gameObject.GetComponent<Player_Status>().use_mp(mp2[character]);
-                }
                 break;
             case 5:
-                if (this.transform.Find("5").gameObject.GetComponent<Player_Status>().get_Now_MP() >= mp2[character])
-                {
-                    //스킬구현
-                    this.transform.Find("5").gameObject.GetComponent<Player_Status>().use_mp(mp2[character]);
-                }
                 break;
         }
     }
