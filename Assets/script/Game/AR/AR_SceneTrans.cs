d1c191c84123c635e66f6b0a3aa370fd30be8820
JungwooKim92 <kimwoo0002@naver.com>
2017-11-06 14:43:25 +0900

    AR_SCENE

---
+++ Assets/script/Game/AR/AR_SceneTrans.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class AR_SceneTrans : MonoBehaviour {
+
+    public void Trans_Win_Main()
+    {
+        SceneManager.LoadScene("scene/Main");
+    }
+
+    public void Trans_Lose_Main()
+    {
+        SceneManager.LoadScene("scene/Main");
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
