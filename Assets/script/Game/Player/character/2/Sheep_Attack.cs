e4dcaeb3bf2920f6535fb6285391c9cdae05ec9e
kitoha <kth004@naver.com>
2017-11-08 00:54:59 +0900

    add ani

--- Assets/script/Game/Player/character/2/Sheep_Attack.cs
+++ Assets/script/Game/Player/character/2/Sheep_Attack.cs
@@ -6,10 +6,12 @@ public class Sheep_Attack : MonoBehaviour {
 
     public int speed;
     int damage;
+	Animator ani;
 
     // Use this for initialization
     void Start()
     {
+		ani = GetComponent<Animator> ();
         damage = PlayerPrefs.GetInt("damage");
     }
 
@@ -22,6 +24,8 @@ public class Sheep_Attack : MonoBehaviour {
     void Update()
     {
         this.gameObject.transform.Translate(0, 0, speed * Time.timeScale * Time.deltaTime);
+		ani.SetFloat("move_x",this.gameObject.transform.position.x);
+		ani.SetFloat("move_y",this.gameObject.transform.position.z);
         if (this.gameObject.transform.position.z > 45)
         {
             Destroy(this.gameObject);
