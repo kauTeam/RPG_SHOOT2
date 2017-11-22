d53afa9f9f551380277a783f5c64f89e5f597caa
kitoha <kth004@naver.com>
2017-11-22 16:35:17 +0900

    update joystick

---
+++ Assets/JoyStick_Change.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoyStick_Change : MonoBehaviour {
+
+	private Transform img;
+	private Transform trs;
+	private Image img2;
+
+	public Canvas parent;
+
+	// Use this for initialization
+	void Start () {
+		img2 = this.GetComponent<Image>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void  FixedUpdate(){
+		if (Input.GetMouseButtonDown(0)) {
+			Vector2 pos2;
+
+			RectTransformUtility.ScreenPointToLocalPointInRectangle (parent.transform as RectTransform, Input.mousePosition, parent.worldCamera, out pos2);
+			img2.rectTransform.anchoredPosition = pos2;
+		}
+
+	}
+}
