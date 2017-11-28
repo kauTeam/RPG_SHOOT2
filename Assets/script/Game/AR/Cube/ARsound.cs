c30e13aa02fd320b28a4a1fb6b4458953a806758
JungwooKim92 <kimwoo0002@naver.com>
2017-11-28 14:26:45 +0900

    sound

---
+++ Assets/script/Game/AR/Cube/ARsound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARsound : MonoBehaviour {
+	public AudioClip BulletClip;
+	public AudioSource BulletSource;
+	public static ARsound soundManager;
+
+	void Awake(){
+		if (ARsound.soundManager == null) {
+			ARsound.soundManager = this;
+		}
+	}
+	void Start(){
+		BulletSource = GetComponent<AudioSource> ();
+	}
+	public void PlaySound(){
+		BulletSource.PlayOneShot (BulletClip);
+	}
+}
