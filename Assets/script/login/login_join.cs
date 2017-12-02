fc32e2f201cf379cecf552eff0e0a1581b3c96b3
zjvlzld <jongsik1995@naver.com>
2017-12-02 22:09:02 +0900

    login_UI

---
+++ Assets/script/login/login_join.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class login_join : MonoBehaviour {
+
+    public GameObject password_panel;
+    public GameObject email_panel;
+    public GameObject enter_panel;
+
+
+    [SerializeField]
+    string email;
+    [SerializeField]
+    string password;
+    [SerializeField]
+    string password_confirm;
+
+    public InputField inputTextEmail;
+    public InputField inputTextPassword;
+    public InputField inputTextPassword_confirm;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void join()
+    {
+        email = inputTextEmail.text;
+        password = inputTextPassword.text;
+        password_confirm = inputTextPassword_confirm.text;
+        print("email"+email);
+        print("password" + password);
+        print("password_confirm" + password_confirm);
+        
+        if (password_confirm == "" || password == "" || email == "")
+        {
+            enter_panel.SetActive(true);
+            return;
+        }
+
+        if (password_confirm!=password)
+        {
+            password_panel.SetActive(true);
+            return;
+        }
+    }
+}
---
+++ Assets/script/login/login_scene_control.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class login_scene_control : MonoBehaviour {
+
+
+    public GameObject email_panel;
+    public GameObject password_panel;
+    public GameObject login_panel;
+    public GameObject enter_panel;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void close()
+    {
+        enter_panel.SetActive(false);
+        email_panel.gameObject.SetActive(false);
+        password_panel.gameObject.SetActive(false);
+        login_panel.gameObject.SetActive(false);
+    }
+}
